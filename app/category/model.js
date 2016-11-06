655978ccfe644b6c2b423c2754b883f8f42ca028
tuckyeah <h.rosebrock@gmail.com>
2016-11-06 11:55:24 -0500

    complete linking UI display to user owned categories

--- app/category/model.js
+++ app/category/model.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 import { hasMany } from 'ember-data/relationships';
-import { belongsTo } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
