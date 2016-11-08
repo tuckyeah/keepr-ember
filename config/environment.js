ef6f69087c3a6dfb362d7b27a8a13744c99b1f8a
tuckyeah <h.rosebrock@gmail.com>
2016-11-08 12:13:52 -0500

    change localhost to updated port

--- config/environment.js
+++ config/environment.js
@@ -33,7 +33,7 @@ module.exports = function (environment) {
     let port = +('GA'.split('').reduce((p, c) =>
       p + c.charCodeAt().toString(16), '')
     );
-    ENV.apiHost = `http://localhost:3000`;
+    ENV.apiHost = `http://localhost:${port}`;
   }
 
   if (environment === 'test') {
